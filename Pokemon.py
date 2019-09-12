ca623431b045c7ed71d7db1a00dbdaaafdd8db9b
ManuelMarinDuque <Manuel.Duque5.2@gmail.com>
2019-09-12 00:16:25 -0500

    Creada la función que asigna aleatoriamente 4 ataques a los 3 pokemones definidos previamente

--- Pokemon.py
+++ Pokemon.py
@@ -9,14 +9,23 @@ def Crear_Pokemon():
 
     for i in range(3):
         valor = random.randint(1,100)
-        print(valor)
         cursor.execute(f"SELECT * FROM Pokemom WHERE Identificator = {valor}")
-        resultado_select = cursor.fetchall()
-        fila = list(resultado_select[0])
+        pokemones_select = cursor.fetchall()
+        fila = list(pokemones_select[0])
+
+        for j in range(4):
+            valor2 = random.randint(1,108)
+            cursor.execute(f"SELECT * FROM Attacks WHERE Identificator = {valor2}")
+            ataques_select = cursor.fetchall()
+            fila2 = list(ataques_select[0])
+            fila.append(fila2)
+
         pokemones.append(fila)
 
     cursor.close()
     conexion.close()
     return pokemones
 
-print(Crear_Pokemon())
+Pokems = Crear_Pokemon()
+print(Pokems)
+
